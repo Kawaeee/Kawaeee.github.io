9aa7cd4446c436fd08197d16d022fcc698e2b9da
Kawaeee <kawaekc@gmail.com>
2020-01-20 01:38:58 +0700

    test seniorproject to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "a96e5cbd97bb7949ce395a3985a0c343",
-"/main.dart.js": "6ba42bcb4697a2c2be1f27072db67ba0",
+"/main.dart.js": "d0baea1ec39ac124bde1823d361a7f38",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "d3b705ed4c9c86b23e11b970b5754f53",
