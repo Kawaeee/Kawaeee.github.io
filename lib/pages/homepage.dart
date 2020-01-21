1e66201839ec7a928486d8717f7f39c72ab0d4cf
Kawaeee <kawaekc@gmail.com>
2020-01-22 00:16:16 +0700

    add desktop site first version

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
                   gradient: theme,
                 ),
               ),
-              screenSize.width < 800
+              screenSize.width < 850
                   ? createMobilePortfolio(constraints)
                   : createDesktopPortfolio(constraints),
             ],
@@ -159,115 +159,209 @@ Widget createMobilePortfolio(dynamic constraints) {
 
 // Desktop site structure
 Widget createDesktopPortfolio(dynamic constraints) {
-  return Container(
-    constraints: BoxConstraints(
-      minWidth: constraints.maxWidth,
-      minHeight: constraints.maxHeight,
-    ),
-    child: Stack(
-      children: <Widget>[
-        Column(
+  return SingleChildScrollView(
+    physics: const AlwaysScrollableScrollPhysics(),
+    child: Container(
+      constraints: BoxConstraints(
+        minWidth: constraints.maxWidth,
+        minHeight: constraints.maxHeight,
+      ),
+      child: IntrinsicHeight(
+        child: Stack(
           children: <Widget>[
-            Row(
+            Column(
               children: <Widget>[
-                Container(
-                  width: constraints.maxWidth - 50,
-                  height: 250,
-                  child: Container(
-                    margin: EdgeInsets.all(16),
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          child: Image.asset(
-                            imgPath,
-                            width: 150,
-                            height: 150,
-                          ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: constraints.maxWidth / 1.1,
+                      height: 250,
+                      child: Container(
+                        margin: EdgeInsets.all(16),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.4),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
                         ),
-                        SizedBox(width: 15),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              SizedBox(height: 40),
-                              Text(
-                                aboutMe['name'],
-                                textAlign: TextAlign.left,
-                                style: name,
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              child: Image.asset(
+                                imgPath,
+                                width: 150,
+                                height: 150,
                               ),
-                              SizedBox(height: 10),
-                              Text(
-                                aboutMe['description'],
-                                textAlign: TextAlign.justify,
-                                style: description,
+                            ),
+                            SizedBox(width: 15),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(height: 40),
+                                  Text(
+                                    aboutMe['name'],
+                                    textAlign: TextAlign.left,
+                                    style: name,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    aboutMe['description'],
+                                    textAlign: TextAlign.justify,
+                                    style: description,
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: constraints.maxWidth / 2,
+                      height: constraints.maxHeight / 1.5,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 16, right: 8, bottom: 32),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.4),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        child: ListView(
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                SizedBox(height: 10),
+                                Text(
+                                  'Education',
+                                  textAlign: TextAlign.center,
+                                  style: sectionHeader,
+                                ),
+                                SizedBox(height: 10),
+                                createSection(education, 'Education'),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Experience',
+                                  textAlign: TextAlign.center,
+                                  style: sectionHeader,
+                                ),
+                                SizedBox(height: 10),
+                                createSection(internship, 'Internship'),
+                                SizedBox(height: 10),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: constraints.maxWidth / 2,
+                      height: constraints.maxHeight / 1.5,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 8, right: 16, bottom: 32),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.4),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        child: ListView(
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                SizedBox(height: 10),
+                                Text(
+                                  'Senior Project',
+                                  textAlign: TextAlign.center,
+                                  style: sectionHeader,
+                                ),
+                                SizedBox(height: 10),
+                                createSection(repositories[0], 'Project'),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Internship Projects',
+                                  textAlign: TextAlign.center,
+                                  style: sectionHeader,
+                                ),
+                                SizedBox(height: 10),
+                                createSection(
+                                    internshipProject[0], 'Internship'),
+                                SizedBox(height: 10),
+                                createSection(
+                                    internshipProject[1], 'Internship'),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Repositories',
+                                  textAlign: TextAlign.center,
+                                  style: sectionHeader,
+                                ),
+                                SizedBox(height: 10),
+                                createSection(repositories[1], 'Project'),
+                                SizedBox(height: 10),
+                                createSection(repositories[2], 'Project'),
+                                SizedBox(height: 10),
+                                createSection(repositories[3], 'Project'),
+                                SizedBox(height: 10),
+                                createSection(repositories[4], 'Project'),
+                                SizedBox(height: 10),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: constraints.maxWidth / 2,
-                  height: 450,
-                  child: Container(
-                    margin: EdgeInsets.only(left: 16, right: 8),
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                    child: Row(),
-                  ),
-                ),
-                Container(
-                  width: constraints.maxWidth / 2,
-                  height: 450,
-                  child: Container(
-                    margin: EdgeInsets.only(left: 8, right: 16),
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                    child: Row(),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-              width: 50,
-              height: 225,
-              margin: EdgeInsets.only(top:16),
-              decoration: BoxDecoration(
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 50,
+                height: 225,
+                margin: EdgeInsets.only(top: 16),
+                decoration: BoxDecoration(
                   color: Color.fromRGBO(255, 255, 255, 0.4),
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30.0),
-                      bottomLeft: Radius.circular(30.0))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  createContact('Phone'),
-                  createContact('Email'),
-                  createContact('GitHub'),
-                  createContact('Linkedin'),
-                  createContact('Medium'),
-                ],
-              )),
+                      bottomLeft: Radius.circular(30.0)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    createContact('Phone'),
+                    createContact('Email'),
+                    createContact('GitHub'),
+                    createContact('Linkedin'),
+                    createContact('Medium'),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                footer,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     ),
   );
 }
