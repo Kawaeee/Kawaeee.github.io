ff76809a374e4e4805fd3601e6dc12bb56761324
Kawaeee <kawaekc@gmail.com>
2020-01-21 23:04:55 +0700

    design desktop site structure - add icon,info,contact

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -13,22 +13,15 @@ class HomePage extends StatelessWidget {
       body: LayoutBuilder(
         builder: (context, constraints) {
           return Stack(
-            children: [
+            children: <Widget>[
               Container(
                 decoration: BoxDecoration(
                   gradient: theme,
                 ),
               ),
-              SingleChildScrollView(
-                physics: const AlwaysScrollableScrollPhysics(),
-                child: Container(
-                  constraints: BoxConstraints(
-                    minWidth: constraints.maxWidth,
-                    minHeight: constraints.maxHeight,
-                  ),
-                  child: createPortfolio(screenSize, context),
-                ),
-              ),
+              screenSize.width < 800
+                  ? createMobilePortfolio(constraints)
+                  : createDesktopPortfolio(constraints),
             ],
           );
         },
@@ -37,137 +30,244 @@ class HomePage extends StatelessWidget {
   }
 }
 
-// Mobile and Desktop site structures
-Widget createPortfolio(Size screenSize, BuildContext context) {
-  if (screenSize.width < 750) {
-    return IntrinsicHeight(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          // About me
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 64),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                // Image
-                Container(
-                  margin: EdgeInsets.only(top: 32),
-                  child: Image.asset(
-                    imgPath,
-                    width: 150,
-                    height: 150,
+// Mobile site structure
+Widget createMobilePortfolio(dynamic constraints) {
+  return SingleChildScrollView(
+    physics: const AlwaysScrollableScrollPhysics(),
+    child: Container(
+      constraints: BoxConstraints(
+        minWidth: constraints.maxWidth,
+        minHeight: constraints.maxHeight,
+      ),
+      child: IntrinsicHeight(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // About me
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 64),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  // Image
+                  Container(
+                    margin: EdgeInsets.only(top: 32),
+                    child: Image.asset(
+                      imgPath,
+                      width: 150,
+                      height: 150,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  aboutMe['name'],
-                  textAlign: TextAlign.center,
-                  style: name,
-                ),
-                SizedBox(height: 8),
-                Text(
-                  aboutMe['description'],
-                  textAlign: TextAlign.justify,
-                  style: description,
-                ),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    createContact('Phone'),
-                    createContact('Email'),
-                    createContact('GitHub'),
-                    createContact('Linkedin'),
-                    createContact('Medium'),
-                  ],
-                ),
-                SizedBox(height: 10),
-                // Education
-                Text(
-                  'Education',
-                  textAlign: TextAlign.center,
-                  style: sectionHeader,
-                ),
-                SizedBox(height: 10),
-                createSection(education, 'Education'),
-                SizedBox(height: 10),
-                // Internship
-                Text(
-                  'Experience',
-                  textAlign: TextAlign.center,
-                  style: sectionHeader,
-                ),
-                SizedBox(height: 10),
-                createSection(internship, 'Internship'),
-                SizedBox(height: 10),
-                // Project
-                Text(
-                  'Senior Project',
-                  textAlign: TextAlign.center,
-                  style: sectionHeader,
-                ),
-                SizedBox(height: 10),
-                createSection(repositories[0], 'Project'),
-                SizedBox(height: 10),
-                Text(
-                  'Internship Projects',
-                  textAlign: TextAlign.center,
-                  style: sectionHeader,
-                ),
-                SizedBox(height: 10),
-                createSection(internshipProject[0], 'Internship'),
-                SizedBox(height: 5),
-                createSection(internshipProject[1], 'Internship'),
-                SizedBox(height: 10),
-                Text(
-                  'Repositories',
-                  textAlign: TextAlign.center,
-                  style: sectionHeader,
-                ),
-                SizedBox(height: 10),
-                createSection(repositories[1], 'Project'),
-                SizedBox(height: 5),
-                createSection(repositories[2], 'Project'),
-                SizedBox(height: 5),
-                createSection(repositories[3], 'Project'),
-                SizedBox(height: 5),
-                createSection(repositories[4], 'Project'),
-                SizedBox(height: 10),
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              footer,
-              style: TextStyle(
-                fontSize: 10,
-                fontWeight: FontWeight.w100,
-                color: Colors.white70,
+                  SizedBox(height: 20),
+                  Text(
+                    aboutMe['name'],
+                    textAlign: TextAlign.center,
+                    style: name,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    aboutMe['description'],
+                    textAlign: TextAlign.justify,
+                    style: description,
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      createContact('Phone'),
+                      createContact('Email'),
+                      createContact('GitHub'),
+                      createContact('Linkedin'),
+                      createContact('Medium'),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  // Education
+                  Text(
+                    'Education',
+                    textAlign: TextAlign.center,
+                    style: sectionHeader,
+                  ),
+                  SizedBox(height: 10),
+                  createSection(education, 'Education'),
+                  SizedBox(height: 10),
+                  // Internship
+                  Text(
+                    'Experience',
+                    textAlign: TextAlign.center,
+                    style: sectionHeader,
+                  ),
+                  SizedBox(height: 10),
+                  createSection(internship, 'Internship'),
+                  SizedBox(height: 10),
+                  // Project
+                  Text(
+                    'Senior Project',
+                    textAlign: TextAlign.center,
+                    style: sectionHeader,
+                  ),
+                  SizedBox(height: 10),
+                  createSection(repositories[0], 'Project'),
+                  SizedBox(height: 10),
+                  Text(
+                    'Internship Projects',
+                    textAlign: TextAlign.center,
+                    style: sectionHeader,
+                  ),
+                  SizedBox(height: 10),
+                  createSection(internshipProject[0], 'Internship'),
+                  SizedBox(height: 5),
+                  createSection(internshipProject[1], 'Internship'),
+                  SizedBox(height: 10),
+                  Text(
+                    'Repositories',
+                    textAlign: TextAlign.center,
+                    style: sectionHeader,
+                  ),
+                  SizedBox(height: 10),
+                  createSection(repositories[1], 'Project'),
+                  SizedBox(height: 5),
+                  createSection(repositories[2], 'Project'),
+                  SizedBox(height: 5),
+                  createSection(repositories[3], 'Project'),
+                  SizedBox(height: 5),
+                  createSection(repositories[4], 'Project'),
+                  SizedBox(height: 10),
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  } else {
-    // Large screen size
-    return Container(
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          "Kawae's portfolio currently support only Mobile site :)",
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w100,
-            color: Colors.white70,
-          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                footer,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
+// Desktop site structure
+Widget createDesktopPortfolio(dynamic constraints) {
+  return Container(
+    constraints: BoxConstraints(
+      minWidth: constraints.maxWidth,
+      minHeight: constraints.maxHeight,
+    ),
+    child: Stack(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  width: constraints.maxWidth - 50,
+                  height: 250,
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Image.asset(
+                            imgPath,
+                            width: 150,
+                            height: 150,
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(height: 40),
+                              Text(
+                                aboutMe['name'],
+                                textAlign: TextAlign.left,
+                                style: name,
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                aboutMe['description'],
+                                textAlign: TextAlign.justify,
+                                style: description,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: constraints.maxWidth / 2,
+                  height: 450,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 16, right: 8),
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    child: Row(),
+                  ),
+                ),
+                Container(
+                  width: constraints.maxWidth / 2,
+                  height: 450,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 8, right: 16),
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    child: Row(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+              width: 50,
+              height: 225,
+              margin: EdgeInsets.only(top:16),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 0.4),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      bottomLeft: Radius.circular(30.0))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  createContact('Phone'),
+                  createContact('Email'),
+                  createContact('GitHub'),
+                  createContact('Linkedin'),
+                  createContact('Medium'),
+                ],
+              )),
+        ),
+      ],
+    ),
+  );
 }
