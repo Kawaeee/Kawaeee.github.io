7dd8a72fa6dc758e60da91738e3cb5458e0d63bd
Kawaeee <kawaekc@gmail.com>
2023-04-03 23:25:26 +0700

    🔨 add base project page

---
+++ src/components/Project.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+defineProps<{
+  name: string,
+  description: string,
+  application_url: string,
+  source_url: string,
+  logo: string
+}>()
+</script>
+
+<template>
+    <div class="project-block">
+      <div class="logo">
+        <img v-bind:src=logo v-bind:alt=name width="75" height="75"/>
+      </div>
+      
+      <div class="description">
+        <h3>{{ name }}</h3>
+        <h4>{{ description }}</h4>
+        <h5 v-if="application_url">
+          <a v-bind:href=application_url>🚀 View Application</a>
+        </h5>
+        <h5 v-if="source_url">
+          <a v-bind:href=source_url v-bind:title="source_url.startsWith('https://github') ? 'View Source on GitHub' : ''">
+            {{ source_url.startsWith('https://github') ? '🔗' : '' }} View Source
+          </a>
+        </h5>
+      </div>
+    </div>
+  </template>
+
+<style scoped>
+
+h3 {
+  font-size: 1rem;
+  font-weight: 600;
+  margin-bottom: 0.25rem;
+  color: var(--color-heading);
+}
+
+h4 {
+  font-size: 0.75rem;
+  font-weight: 400;
+  margin-bottom: 0.25rem;
+  color: var(--color-text);
+}
+
+h5 {
+  font-size: 0.75rem;
+  font-weight: 300;
+  margin-bottom: 0.25rem;
+  color: var(--color-text);
+}
+
+.project-block {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  display: flex;
+  margin-right: 15px;
+  place-items: center;
+  place-content: center;
+  border: 1px solid var(--color-border);
+  border-radius: 8px;
+  width: 100px;
+  height: 100px;
+  color: var(--color-text);
+}
+
+.description {
+  flex: 1;
+}
+
+</style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,15 +1,34 @@
+<script setup lang="ts">
+import Project from '../components/Project.vue'
+const projects = [
+  { name: "Corgi butt or a loaf of bread?", description: "🍞🐶 A classifier that can classify between corgi butt and loaf of bread using PyTorch", application_url: "https://share.streamlit.io/kawaeee/butt_or_bread/", logo: "src/assets/projects/butt_or_bread.png", source_url: "https://github.com/Kawaeee/butt_or_bread" },
+  { name: "Scala DS", description: "📊💻🔢 Scala programming language for data science!", application_url: "https://kawaeee.github.io/scala_ds/", logo: "src/assets/projects/scala.png", source_url: "https://github.com/Kawaeee/scala_ds" },
+  { name: "iStylist: Hairstyle Recommender", description: "💇‍♂️💻📱 The mobile application that helps the user to choose a hairstyle based on the user's face shape", application_url: "https://seniorproject.sit.kmutt.ac.th/showproject/CS59-RE43", logo: "src/assets/projects/istylist.png", source_url: "" },
+  { name: "py-hLEPOR", description: "🐍📊 Python Bindings to hLEPOR metric", application_url: "", logo: "src/assets/projects/blank.png", source_url: "https://github.com/Kawaeee/py-hLEPOR" },
+  { name: "pHash", description: "🔍🖼️ Image de-duplication using Perceptual Hashing(pHash)", application_url: "", logo: "src/assets/projects/blank.png", source_url: "https://github.com/Kawaeee/phash" },
+  { name: "For other projects", description: "📁🔍 Feel free to check in my GitHub!", application_url: "", logo: "src/assets/projects/github.png", source_url: "https://github.com/Kawaeee/" },
+]
+</script>
+
 <template>
-  <div class="project">
-    <h1>This is an project page</h1>
+  <div class="projects">
+    <div class="grid">
+      <Project v-for="(project, index) in projects" :key="index" :name="project.name" :description="project.description" :application_url="project.application_url" :logo="project.logo" :source_url="project.source_url" />
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .project {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="scss">
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+  padding: 20px;
+}
+
+@media (max-width: 1024px) {
+  .grid {
+    grid-template-columns: repeat(1, 1fr);
   }
 }
 </style>
