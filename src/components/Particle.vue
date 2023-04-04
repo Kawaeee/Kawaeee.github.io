4056f2ccf1b7473321e1bb2ff719624cf00c3c25
Kawaeee <kawaekc@gmail.com>
2023-04-04 16:18:15 +0700

    🔨 integrate particle with tariap face test

--- src/components/Particle.vue
+++ src/components/Particle.vue
@@ -5,48 +5,562 @@ import { loadSlim } from "tsparticles-slim";
 export default {
   data() {
     return {
-      // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
       options: {
-        fullScreen: {
-          enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
-          zIndex: -1, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
-        },
+        autoplay: true,
+        delay: 0,
+        fullScreen: { enable: true, zIndex: 1, },
+        detectRetina: true,
+        duration: 0,
+        fpsLimit: 120,
         interactivity: {
           events: {
-            onClick: {
-              enable: true, // enables the click event, it's disabled by default
-              mode: "push", // adds the particles on click
-            },
-            onHover: {
-              enable: true, // enables the mouse hover event, it's disabled by default
-              mode: "repulse", // repulse the particles on mouse hover
-            },
+            detectsOn: "window",
+            onClick: { enable: true, mode: "push", },
+            // onDiv: { selectors: "#repulse-div", enable: false, mode: "repulse", type: "circle", },
+            onHover: { enable: true, mode: "bubble", parallax: { enable: false, force: 60, smooth: 10, }},
+            resize: { delay: 0.5, enable: true, }
           },
           modes: {
+            attract: {
+              distance: 200,
+              duration: 0.4,
+              easing: "ease-out-quad",
+              factor: 1,
+              maxSpeed: 50,
+              speed: 1,
+            },
+            bounce: {
+              distance: 200
+            },
+            bubble: {
+              distance: 400,
+              duration: 2,
+              mix: false,
+              opacity: 0.8,
+              size: 40,
+              divs: {
+                distance: 200,
+                duration: 0.4,
+                mix: false,
+                selectors: [],
+              }
+            },
+            connect: {
+              distance: 80,
+              links: {
+                opacity: 0.5,
+              },
+              radius: 60,
+            },
+            grab: {
+              distance: 400,
+              links: {
+                blink: false,
+                consent: false,
+                opacity: 1,
+              }
+            },
             push: {
-              quantity: 10, // number of particles to add on click
+              default: true,
+              groups: [],
+              quantity: 3,
+            },
+            remove: {
+              quantity: 2,
             },
             repulse: {
-              distance: 300, // distance of the particles from the cursor
+              distance: 200,
+              duration: 0.4,
+              factor: 100,
+              speed: 1,
+              maxSpeed: 50,
+              easing: "ease-out-quad",
+              divs: {
+                distance: 200,
+                duration: 0.4,
+                factor: 100,
+                speed: 1,
+                maxSpeed: 50,
+                easing: "ease-out-quad",
+                selectors: [],
+              }
             },
+            slow: {
+              factor: 3,
+              radius: 200,
+            },
+            trail: {
+              delay: 1,
+              pauseOnStop: false,
+              quantity: 1,
+            },
+            light: {
+              area: {
+                gradient: {
+                  start: {
+                    value: "#ffffff",
+                  },
+                  stop: {
+                    value: "#000000",
+                  }
+                },
+                radius: 1000,
+              },
+              shadow: {
+                color: {
+                  value: "#000000",
+                },
+                length: 2000,
+              }
+            }
           },
         },
         particles: {
-          links: {
-            distance: 200, // maximum distance for linking the particles
-            enable: true, // enabling this will make the particles linked together
+          bounce: {
+            horizontal: {
+              random: {
+                enable: false,
+                minimumValue: 0.1,
+              },
+              value: 1,
+            },
+            vertical: {
+              random: {
+                enable: false,
+                minimumValue: 0.1,
+              },
+              value: 1,
+            }
+          },
+          collisions: {
+            absorb: {
+              speed: 2,
+            },
+            bounce: {
+              horizontal: {
+                random: {
+                  enable: false,
+                  minimumValue: 0.1,
+                },
+                value: 1,
+              },
+              vertical: {
+                random: {
+                  enable: false,
+                  minimumValue: 0.1,
+                },
+                value: 1,
+              }
+            },
+            enable: false,
+            mode: "bounce",
+            overlap: {
+              enable: true,
+              retries: 0,
+            }
+          },
+          color: {
+            value: "#123456", // coloring
+            animation: {
+              h: {
+                count: 0,
+                enable: false,
+                offset: 0,
+                speed: 1,
+                decay: 0,
+                sync: true,
+              },
+              s: {
+                count: 0,
+                enable: false,
+                offset: 0,
+                speed: 1,
+                decay: 0,
+                sync: true,
+              },
+              l: {
+                count: 0,
+                enable: false,
+                offset: 0,
+                speed: 1,
+                decay: 0,
+                sync: true,
+              }
+            }
           },
           move: {
-            enable: true, // enabling this will make particles move in the canvas
-            speed: { min: 1, max: 5 }, // using a range in speed value will make particles move in a random speed between min/max values, each particle has its own value, it won't change over time by default
+            angle: {
+              offset: 0,
+              value: 90,
+            },
+            attract: {
+              distance: 200,
+              enable: false,
+              rotate: {
+                x: 600,
+                y: 1200,
+              }
+            },
+            center: {
+              x: 50,
+              y: 50,
+              mode: "percent",
+              radius: 0,
+            },
+            decay: 0,
+            distance: {},
+            direction: "none",
+            drift: 0,
+            enable: true,
+            gravity: {
+              acceleration: 9.81,
+              enable: false,
+              inverse: false,
+              maxSpeed: 50,
+            },
+            path: {
+              clamp: true,
+              delay: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 0,
+              },
+              enable: false,
+              options: {},
+            },
+            outModes: {
+              default: "out",
+              bottom: "out",
+              left: "out",
+              right: "out",
+              top: "out",
+            },
+            random: false,
+            size: false,
+            speed: 2,
+            spin: {
+              acceleration: 0,
+              enable: false,
+            },
+            straight: false,
+            trail: {
+              enable: false,
+              length: 10,
+              fill: {},
+            },
+            vibrate: false,
+            warp: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              width: 1920,
+              height: 1080,
+            },
+            limit: 0,
+            value: 80,
           },
           opacity: {
-            value: { min: 0.3, max: 0.7 }, // using a different opacity, to have some semitransparent effects
+            random: {
+              enable: true,
+              minimumValue: 0.1,
+            },
+            value: {
+              min: 0.1,
+              max: 1,
+            },
+            animation: {
+              count: 0,
+              enable: true,
+              speed: 1,
+              decay: 0,
+              sync: false,
+              destroy: "none",
+              startValue: "random",
+              minimumValue: 0.2,
+            }
+          },
+          reduceDuplicates: false,
+          shadow: {
+            blur: 0,
+            color: {
+              value: "#000"
+            },
+            enable: false,
+            offset: {
+              x: 0,
+              y: 0,
+            }
+          },
+          shape: {
+            options: {
+              character: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400",
+              },
+              char: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400",
+              },
+              polygon: {
+                sides: 5,
+              },
+              star: {
+                sides: 5,
+              },
+              // need to change!
+              image: [
+                {
+                  src: "https://avatars.githubusercontent.com/u/77453675?s=200&v=4",
+                  width: 32,
+                  height: 32,
+                },
+              ],
+              images: [
+                {
+                  src: "https://avatars.githubusercontent.com/u/77453675?s=200&v=4",
+                  width: 32,
+                  height: 32,
+                },
+              ]
+            },
+            type: "image",
           },
           size: {
-            value: { min: 1, max: 3 }, // let's randomize the particles size a bit
+            random: {
+              enable: false,
+              minimumValue: 1,
+            },
+            value: 16,
+            animation: {
+              count: 0,
+              enable: false,
+              speed: 40,
+              decay: 0,
+              sync: false,
+              destroy: "none",
+              startValue: "random",
+              minimumValue: 0.1,
+            }
           },
+          stroke: {
+            width: 0,
+            color: {
+              value: "#000000",
+              animation: {
+                h: {
+                  count: 0,
+                  enable: false,
+                  offset: 0,
+                  speed: 1,
+                  decay: 0,
+                  sync: true,
+                },
+                s: {
+                  count: 0,
+                  enable: false,
+                  offset: 0,
+                  speed: 1,
+                  decay: 0,
+                  sync: true,
+                },
+                l: {
+                  count: 0,
+                  enable: false,
+                  offset: 0,
+                  speed: 1,
+                  decay: 0,
+                  sync: true,
+                }
+              }
+            }
+          },
+          zIndex: {
+            random: {
+              enable: false,
+              minimumValue: 0,
+            },
+            value: 0,
+            opacityRate: 1,
+            sizeRate: 1,
+            velocityRate: 1,
+          },
+          life: {
+            count: 0,
+            delay: {
+              random: {
+                enable: false,
+                minimumValue: 0,
+              },
+              value: 0,
+              sync: false,
+            },
+            duration: {
+              random: {
+                enable: false,
+                minimumValue: 0.0001,
+              },
+              value: 0,
+              sync: false,
+            }
+          },
+          rotate: {
+            random: {
+              enable: true,
+              minimumValue: 0,
+            },
+            value: 0,
+            animation: {
+              enable: true,
+              speed: 5,
+              decay: 0,
+              sync: false,
+            },
+            direction: "random",
+            path: false,
+          },
+          destroy: {
+            bounds: {},
+            mode: "none",
+            split: {
+              count: 1,
+              factor: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 3,
+              },
+              rate: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: {
+                  min: 4,
+                  max: 9,
+                }
+              },
+              sizeOffset: true,
+              particles: {},
+            }
+          },
+          roll: {
+            darken: {
+              enable: false,
+              value: 0,
+            },
+            enable: false,
+            enlighten: {
+              enable: false,
+              value: 0,
+            },
+            mode: "vertical",
+            speed: 25,
+          },
+          tilt: {
+            random: {
+              enable: false,
+              minimumValue: 0,
+            },
+            value: 0,
+            animation: {
+              enable: false,
+              speed: 0,
+              decay: 0,
+              sync: false,
+            },
+            direction: "clockwise",
+            enable: false,
+          },
+          twinkle: {
+            lines: {
+              enable: false,
+              frequency: 0.05,
+              opacity: 1,
+            },
+            particles: {
+              enable: false,
+              frequency: 0.05,
+              opacity: 1,
+            }
+          },
+          wobble: {
+            distance: 5,
+            enable: false,
+            speed: {
+              angle: 50,
+              move: 10,
+            }
+          },
+          orbit: {
+            animation: {
+              count: 0,
+              enable: false,
+              speed: 1,
+              decay: 0,
+              sync: false,
+            },
+            enable: false,
+            opacity: 1,
+            rotation: {
+              random: {
+                enable: false,
+                minimumValue: 0,
+              },
+              value: 45,
+            },
+            width: 1,
+          },
+          links: {
+            blink: false,
+            color: {
+              value: "#000"
+            },
+            consent: false,
+            distance: 150,
+            enable: false,
+            frequency: 1,
+            opacity: 0.4,
+            shadow: {
+              blur: 5,
+              color: {
+                value: "#000",
+              },
+              enable: false,
+            },
+            triangles: {
+              enable: false,
+              frequency: 1,
+            },
+            width: 1,
+            warp: false,
+          },
+          repulse: {
+            random: {
+              enable: false,
+              minimumValue: 0,
+            },
+            value: 0,
+            enabled: false,
+            distance: 1,
+            duration: 1,
+            factor: 1,
+            speed: 1,
+          }
         },
+        pauseOnBlur: true,
+        pauseOnOutsideViewport: true,
+        smooth: true,
+        zLayers: 100,
       },
     };
   },
@@ -59,5 +573,9 @@ export default {
 </script>
 
 <template>
-  <Particles id="tsparticles" v-bind:options="options" v-bind:particlesInit="particlesInit"/>
+  <Particles
+    id="tsparticles"
+    v-bind:options="options"
+    v-bind:particlesInit="particlesInit"
+  />
 </template>
