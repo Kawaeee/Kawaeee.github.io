d714543fcc78176d23604b1f064dedf242b7b9bb
Kasidech C. <kawaekc@gmail.com>
2025-01-04 15:15:10 +0000

    🚧wip deployment

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs', // Ensure the build output is in the `docs` folder
-  basePath: '/Kawaeee.github.io',
-  assetPrefix: '/Kawaeee.github.io/',
   eslint: {
     ignoreDuringBuilds: true,
   },
