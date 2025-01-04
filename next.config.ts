65fb68495138218513b7a3b7463e7edfdf6e87e0
Kasidech C. <kawaekc@gmail.com>
2025-01-04 14:59:18 +0000

    💚fix build process

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: 'docs', // Ensure the build output is in the `docs` folder
+  basePath: '/Kawaeee.github.io',
+  assetPrefix: '/Kawaeee.github.io/',
   eslint: {
     ignoreDuringBuilds: true,
   },
